a4b88be9694e95f169f35da0d291a9f5e216c921
Guillemot Moussu <guillemot.moussu@gmail.com>
2023-03-21 00:41:12 +0100

    avancée json

--- TP4/Json.hs
+++ TP4/Json.hs
@@ -1,4 +1,27 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+import Data.Array (Array)
+{-# HLINT ignore "Use camelCase" #-}
 
- -- coming soon
+data JSON =
+    JSON_Int Integer |
+    JSON_Bool Bool |
+    JSON_String String |
+    JSON_Array [JSON] |
+    JSON_Object [(String, JSON)]
 
- -- Jaaj
+true :: Bool
+true = True
+
+false :: Bool
+false = False
+
+exemple_tableau :: [JSON]
+exemple_tableau = [JSON_Int 42, JSON_Bool true, JSON_String "JSON!", JSON_Array [false, 0]]
+
+exemple_objet :: [(String, JSON)]
+exemple_objet = [("entier",42), ("texte","Haskell!"), ("tableau",[true])]
+
+-- Cela fait penser à la structure de données Bintree
+
+show_json :: JSON -> String
+show_json = 
